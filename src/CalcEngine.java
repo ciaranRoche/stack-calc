aea86a3c0c28c2b39a676078c510c1a6013d3e8a
ciaran <ciaranroche@hotmail.co.uk>
2017-01-27 13:40:19 +0000

    first commit

---
+++ src/CalcEngine.java
@@ -0,0 +1,166 @@
+import java.util.Stack;
+
+/**
+ * The main part of the calculator doing the calculations.
+ *
+ * @author  M. Kolling
+ * @version 0.1 (incomplete)
+ * @editedBy Ciaran Roche
+ * @date 23/01/2017
+ */
+public class CalcEngine
+{
+    public String output;
+    public Stack<Integer> memory;
+    public Stack<Character>stack;
+
+
+
+    public String ans;
+    /**
+     * Create a CalcEngine instance. Initialise its state so that it is ready
+     * for use.
+     */
+    public CalcEngine()
+    {
+        stack = new Stack<Character>();
+        memory = new Stack<Integer>();
+        output = "";
+        ans = "";
+    }
+
+    public void calculate(String output){
+        memory.clear();
+        ans = "";
+        for(int n=0; n<output.length(); n++){
+            int ch = output.charAt(n);
+            if('0'<=ch && ch<='9')
+                memory.push(ch - '0');
+            else{
+                switch(ch){
+                    case '+':
+                        int ba = memory.pop();
+                        int aa = memory.pop();
+                        memory.push(aa+ba);
+                        break;
+                    case '-':
+                        int bm = memory.pop();
+                        int am = memory.pop();
+                        memory.push(am-bm);
+                        break;
+                    case '*':
+                        int bmu = memory.pop();
+                        int amu = memory.pop();
+                        memory.push(amu*bmu);
+                        break;
+                    case '/':
+                        int bd = memory.pop();
+                        int ad = memory.pop();
+                        memory.push(ad/bd);
+                        break;
+                }
+            }
+        }
+        ans = ans + memory.pop();
+    }
+
+    public String infixToPostfix(String s){
+        for(int i = 0; i< s.length(); i++){
+            char ch = s.charAt(i);
+            switch (ch){
+                case '+':
+                case '-':
+                    operator(ch, 1);
+                    break;
+                case '*':
+                case '/':
+                    operator(ch, 2);
+                    break;
+                case '(':
+                    stack.push(ch);
+                    break;
+                case ')':
+                    priority(ch);
+                    break;
+                default:
+                    output = output + ch;
+                    break;
+            }
+        }
+        while(!stack.isEmpty()){
+            output = output + stack.pop();
+        }
+        System.out.println(output);
+        calculate(output);
+        return output;
+
+    }
+
+    public void operator(char opThis, int prec1){
+        while(!stack.isEmpty()){
+            char topStack = stack.pop();
+            if (topStack == '('){
+                stack.push(topStack);
+                break;
+            }else{
+                int prec2;
+                if(topStack == '+' || topStack == '-')
+                    prec2 = 1;
+                else
+                    prec2 = 2;
+                if(prec2 < prec1){
+                    stack.push(topStack);
+                    break;
+                }else output = output + topStack;
+            }
+        }
+        stack.push(opThis);
+    }
+
+    public void priority(char ch){
+        while(!stack.isEmpty()){
+            char cha = stack.pop();
+            if (cha == '(')
+                break;
+            else {output = output + cha;
+                //stack.clear();
+            }
+        }
+    }
+
+    public void clear(){
+        output = "";
+    }
+
+    public String getOutput(){
+        return output;
+    }
+
+
+    /**
+     * Return the title of this calculation engine.
+     */
+    public String getTitle()
+    {
+        return("Not So Scientific Calculator");
+    }
+
+    /**
+     * Return the author of this engine. This string is displayed as it is,
+     * so it should say something like "Written by H. Simpson".
+     */
+    public String getAuthor()
+    {
+        return("Ciaran Roche");
+    }
+
+    /**
+     * Return the version number of this engine. This string is displayed as
+     * it is, so it should say something like "Version 1.1".
+     */
+    public String getVersion()
+    {
+        return("Ver. 1.0");
+    }
+
+}
---
+++ src/UserInterface.java
@@ -0,0 +1,171 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.*;
+
+/**
+ * A graphical user interface for the calculator. No calculation is being
+ * done here. This class is responsible just for putting up the display on
+ * screen. It then refers to the "CalcEngine" to do all the real work.
+ *
+ * @author Michael Kolling
+ * @version 31 July 2000
+ * @editedBy Ciaran Roche
+ * @date 23/01/2017
+ */
+public class UserInterface
+        implements ActionListener
+{
+    private CalcEngine calc;
+    private boolean showingAuthor;
+    private String infix;
+
+    private JFrame frame;
+    private JTextField display, displayAns;
+    private JLabel status;
+
+    /**
+     * Create a user interface for a given calcEngine.
+     */
+    public UserInterface(CalcEngine engine)
+    {
+        calc = engine;
+        infix = "";
+        showingAuthor = true;
+        makeFrame();
+        frame.setVisible(true);
+    }
+
+    /**
+     * Make this interface visible again. (Has no effect if it is already
+     * visible.)
+     */
+    public void setVisible(boolean visible)
+    {
+        frame.setVisible(visible);
+    }
+
+    /**
+     * Make the frame for the user interface.
+     */
+    private void makeFrame()
+    {
+        frame = new JFrame(calc.getTitle());
+
+        JPanel contentPane = (JPanel)frame.getContentPane();
+        contentPane.setLayout(new BorderLayout(10, 10));
+        contentPane.setBackground(Color.darkGray);
+        contentPane.getFont().isBold();
+        contentPane.setBorder(new EmptyBorder( 10, 10, 10, 10));
+
+        display = new JTextField();
+        contentPane.add(display, BorderLayout.NORTH);
+
+        JPanel buttonPanel = new JPanel(new GridLayout(5, 4));
+        addButton(buttonPanel, "C");
+        addButton(buttonPanel, "(");
+        addButton(buttonPanel, ")");
+        addButton(buttonPanel, "/");
+        addButton(buttonPanel, "7");
+        addButton(buttonPanel, "8");
+        addButton(buttonPanel, "9");
+        addButton(buttonPanel, "*");
+        addButton(buttonPanel, "4");
+        addButton(buttonPanel, "5");
+        addButton(buttonPanel, "6");
+        addButton(buttonPanel, "+");
+        addButton(buttonPanel, "1");
+        addButton(buttonPanel, "2");
+        addButton(buttonPanel, "3");
+        addButton(buttonPanel, "-");
+        addButton(buttonPanel, "0");
+        addButton(buttonPanel, ".");
+        addButton(buttonPanel, "^");
+        addButton(buttonPanel, "=");
+        contentPane.add(buttonPanel, BorderLayout.CENTER);
+
+//		status = new JLabel(calc.getAuthor());
+//		contentPane.add(status,BorderLayout.PAGE_END);
+        displayAns = new JTextField();
+        contentPane.add(displayAns, BorderLayout.SOUTH);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+    }
+
+    /**
+     * Add a button to the button panel.
+     */
+    private void addButton(Container panel, String buttonText)
+    {
+        JButton button = new JButton(buttonText);
+        button.addActionListener(this);
+        panel.add(button);
+    }
+
+    /**
+     * An interface action has been performed. Find out what it was and
+     * handle it.
+     */
+    public void actionPerformed(ActionEvent event)
+    {
+        String command = event.getActionCommand();
+
+        if(command.equals("0") ||
+                command.equals("1") ||
+                command.equals("2") ||
+                command.equals("3") ||
+                command.equals("4") ||
+                command.equals("5") ||
+                command.equals("6") ||
+                command.equals("7") ||
+                command.equals("8") ||
+                command.equals("9") ||
+                command.equals("^") ||
+                command.equals("(") ||
+                command.equals(")") ||
+                command.equals(".") ||
+                command.equals("+") ||
+                command.equals("-") ||
+                command.equals("*") ||
+                command.equals("/"))
+        {
+            infix = infix.concat(command);
+        }
+        else if(command.equals("=")){
+            calc.infixToPostfix(infix);
+            ansDisplay();
+        }
+        else if(command.equals("C"))
+            infix = "";
+        calc.clear();
+        redisplay();
+    }
+
+    /**
+     * Update the interface display to show the current value of the
+     * calculator.
+     */
+    private void redisplay()
+    {
+        display.setText(infix);
+    }
+
+    private void ansDisplay(){
+        displayAns.setText(calc.ans);
+    }
+
+    /**
+     * Toggle the info display in the calculator's status area between the
+     * author and version information.
+     */
+    private void showInfo()
+    {
+        if(showingAuthor)
+            status.setText(calc.getVersion());
+        else
+            status.setText(calc.getAuthor());
+
+        showingAuthor = !showingAuthor;
+    }
+}
